25b75292a9f6a26bc23a0cd3cf73561c9e4026e3
WoKop <75836571+wojciechkoperny@users.noreply.github.com>
2024-11-15 10:25:50 +0100

    Add files via upload
    initial commit

---
+++ inc/header.h
@@ -0,0 +1,11 @@
+#ifndef HEADER
+#define HEADER
+#define RET_VAL 0
+#define PRINT_VAL 42
+
+extern int GlobalVarA;
+static int StaticVarA = 22;
+
+void test_interface(void);
+
+#endif
--- other_file.c
+++ other_file.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
 #include "header.h"
-
-int GlobalVarA = 11;
-
-int main() {
-    GlobalVarA++;
-    StaticVarA++;
+void test_interface(void)
+{
    printf("Hello, World \n GlobalVar: %d\n StaticVar %d!\n\n", GlobalVarA,StaticVarA);
-   test_interface();
-   return 0;
 }
